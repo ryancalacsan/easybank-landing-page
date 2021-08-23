387891e6286d3a35838339558554942246e87269
ryancalacsan <ryancalacsan@gmail.com>
2021-08-22 23:44:40 -0500

    added animated hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function () {
+  console.log('open hamburger');
+  header.classList.toggle('open');
+
+  if (header.classList.contains('open')) {
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else {
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+});
