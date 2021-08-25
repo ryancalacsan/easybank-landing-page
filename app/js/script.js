8d057675f1c0124acf335b7d19e6526e0a36d964
ryancalacsan <ryancalacsan@gmail.com>
2021-08-24 21:36:54 -0500

    added fade animation to mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function () {
-  console.log('open hamburger');
-  header.classList.toggle('open');
-
   if (header.classList.contains('open')) {
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(el => {
+      el.classList.remove('fade-in');
+      el.classList.add('fade-out');
+    });
   } else {
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(el => {
+      el.classList.remove('fade-out');
+      el.classList.add('fade-in');
+    });
   }
 });
